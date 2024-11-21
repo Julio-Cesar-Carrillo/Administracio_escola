00af8243cfac3ebc2768468f1c2236ebbb40999b
Darckfer <juliocesarcarrillorocha@gmail.com>
2024-11-21 18:09:42 +0100

    todo

---
+++ views/procesos/confirmareditar.php
@@ -0,0 +1,28 @@
+<?php
+
+include './conexion.php';
+
+$id_alumno = mysqli_real_escape_string($conn, $_POST['id_alumno']);
+$dni_alu = mysqli_real_escape_string($conn, $_POST['dni_alu']);
+$nom_alu = mysqli_real_escape_string($conn, $_POST['nom_alu']);
+$cognom1_alu = mysqli_real_escape_string($conn, $_POST['cognom1_alu']);
+$cognom2_alu = mysqli_real_escape_string($conn, $_POST['cognom2_alu']);
+$telf_alu = mysqli_real_escape_string($conn, $_POST['telf_alu']);
+$email_alu = mysqli_real_escape_string($conn, $_POST['email_alu']);
+$id_curso = mysqli_real_escape_string($conn, $_POST['id_curso']);
+
+try {
+    $sqlaceptar = "UPDATE tbl_alumnos 
+    SET dni_alu = ? , nom_alu = ?, cognom1_alu = ?, cognom2_alu = ?, telf_alu = ?, email_alu = ?, id_curso = ?
+    WHERE id_alumno = ?";
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_prepare($stmt, $sqlaceptar);
+    mysqli_stmt_bind_param($stmt, "ssssssii", $dni_alu, $nom_alu, $cognom1_alu, $cognom2_alu, $telf_alu, $email_alu, $id_curso, $id_alumno);
+    mysqli_stmt_execute($stmt);
+    mysqli_commit($conn);
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+} catch (Exception $e) {
+    mysqli_rollback($conn);
+    echo "Error: " . $e->getMessage();
+}
---
+++ views/procesos/eliminar.php
@@ -0,0 +1,29 @@
+<?php
+include './conexion.php';
+$id_alumno = $_POST['id'];
+try {
+    // Iniciar transacción
+    mysqli_autocommit($conn, false);
+    mysqli_begin_transaction($conn, MYSQLI_TRANS_START_READ_WRITE);
+
+    // Eliminar historial de chat
+    $sqlChat = "DELETE FROM tbl_notas WHERE id_alumno = ?";
+    $stmtChat = mysqli_prepare($conn, $sqlChat);
+    mysqli_stmt_bind_param($stmtChat, "i", $id_alumno);
+    mysqli_stmt_execute($stmtChat);
+    mysqli_stmt_close($stmtChat);
+
+    // Eliminar amistad
+    $sql = "DELETE FROM tbl_alumnos WHERE id_alumno = ?";
+    $stmt = mysqli_prepare($conn, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $id_alumno);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    // Commit de la transacción
+    mysqli_commit($conn);
+} catch (Exception $e) {
+    mysqli_rollback($conn);
+    echo $e->getMessage();
+}
+mysqli_close($conn);
