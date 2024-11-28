f20b16c22470c508f3504386cf1a7e7d6f73bfb4
Darckfer <juliocesarcarrillorocha@gmail.com>
2024-11-28 16:40:52 +0100

    todo funcional

--- views/editar.php
+++ views/editar.php
@@ -16,7 +16,7 @@ $id_alumno = $_POST['id'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Alumno</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="./js/validaciones.js"></script>
+    <script src="../js/validaciones.js"></script>
 </head>
 
 <body class="bg-light">
